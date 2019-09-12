7a17e81e71c511eff84d28b4d2cd4e4c00674383
Robert Freeman <robertisafreeman@gmail.com>
2019-09-11 20:25:46 -0700

    displaying json data

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-console.log(path.join(__dirname, 'index.html'));
 app.get('/', (req, res, next)=>{
     res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+app.get('/api/products', (req, res, next)=> {
+    res.sendFile(path.join(__dirname, 'products.json'))
+})
+
 
 app.listen(3000, ()=> console.log('listening on port 3000'));
