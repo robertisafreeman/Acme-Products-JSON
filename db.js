e2df47dce4c13789b05c04d4f28201976dc12352
Robert Freeman <robertisafreeman@gmail.com>
2019-09-11 20:53:50 -0700

    struggling with post currently

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const FILE = './products.json';
 
 
 module.exports = (FILE, validator) => {
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const db = require('./db')
+
 
 app.get('/', (req, res, next)=>{
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/api/products', (req, res, next)=> {
+    try{
     res.sendFile(path.join(__dirname, 'products.json'))
-})
-
+    }
+    catch(ex){
+        next(ex);
+    }
+});
 
 app.listen(3000, ()=> console.log('listening on port 3000'));
